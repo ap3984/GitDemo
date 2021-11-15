627229e77eee464c25ee8a16b202fcb3fe11ba7a
Abhishek Pandey <abhishek.p@pyramidconsultinginc.com>
2021-11-15 20:34:28 +0530

    first commit

---
+++ src/test/java/MavenTraining/MavenOne/AppiumTest.java
@@ -0,0 +1,20 @@
+package MavenTraining.MavenOne;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSapps");
+		
+	}
+
+}
